fd9dc6a35127672920fcad402805b39fa4830984
Ammar Baig <78850248+Ammar-Baig@users.noreply.github.com>
2023-11-15 20:19:04 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     setLoading(true); // Set loading state to true
 
     const imageSrc = webcamRef.current.getScreenshot();
-    const backendEndpoint = 'http://143.244.137.248:8000/upload-image/';
+    const backendEndpoint = 'https://avantskincare.co.uk/upload-image/';
 
     try {
       const formData = new FormData();
